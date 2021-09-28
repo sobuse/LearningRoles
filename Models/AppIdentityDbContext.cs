e3da623dcd5446a3df6479cb7090e6c750befe09
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-09-28 06:51:01 +0100

    Add project files.

---
+++ Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningRoles.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) { }
+    }
+}
